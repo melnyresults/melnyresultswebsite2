f6149799a046ecd07749b82ff0ab814d9ac1ecd1
melnyresults <ivanmelnychenkoo@gmail.com>
2025-07-07 00:00:33 -0400

    Updated LoginPage.tsx

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -72,6 +72,15 @@ const LoginPage: React.FC = () => {
                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:text-primary-blue focus:outline-none"
               >
                 {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile Menu */}
+        {isMobileMenuOpen && (
+          <div className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
               <Link
                 to="/"
                 className="block px-3 py-2 text-gray-900 hover:text-primary-blue w-full text-left font-medium"
